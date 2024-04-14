e359fc292ff952d43fd40473039293e7dbb4f548
My <uk472363@yanex.ru>
2024-04-14 18:37:14 +0300

    Initial Commit

---
+++ dcssc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dcssc
+//
+//  Created by user on 14.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var mkk: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
